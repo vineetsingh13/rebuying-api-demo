508bb67227ba93516c682453af3d9ac5659f1418
Vineet Singh <singhvineet13502@gmail.com>
2023-08-26 12:27:15 +0530

    removed the cors headers

--- rebuy/settings.py
+++ rebuy/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
     'rest_framework',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -54,13 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "https://web-production-dae5.up.railway.app",
-    # Add more allowed origins if needed
-]
 
 ROOT_URLCONF = 'rebuy.urls'
 
